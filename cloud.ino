0ffcea0ce78a16371316b47e5ab4259021b1cf87
Mayur <55876994+mayurm03@users.noreply.github.com>
2020-03-10 17:37:10 +0000

    Add files via upload

---
+++ cloud.ino
@@ -0,0 +1,50 @@
+#include <ssl_client.h>
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <cert.h>
+ 
+char SSID[] = "Shubham's iPhone";
+const char *PWD = "shubh7482";
+ 
+//WiFiClientSecure secureClient = WiFiClientSecure();
+//PubSubClient mqttClient(secureClient);
+ 
+void connectToWiFi() {
+  Serial.print("Connecting to ");
+  Serial.println(SSID);
+  WiFi.begin(SSID, PWD); 
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+    // we can even make the ESP32 to sleep
+  }
+ 
+  Serial.print("Connected - ");
+  
+}
+/* 
+void connectToAWS() {
+  mqttClient.setServer("http://a3jjkbw4vqw4wh-ats.iot.us-east-1.amazonaws.com/", 8883);
+  secureClient.setCACert(AWS_PUBLIC_CERT);
+  secureClient.setCertificate(AWS_DEVICE_CERT);
+  secureClient.setPrivateKey(AWS_PRIVATE_KEY);
+ 
+  Serial.println("Connecting to MQTT....");
+ 
+  mqttClient.connect("Irrigation");
+   
+  while (!mqttClient.connected()) {
+    Serial.println("Connecting to MQTT....Retry");
+    mqttClient.connect("Irrigation");
+    delay(5000);
+  }
+ 
+  Serial.println("MQTT Connected");
+}*/
+void loop (){}
+void setup() {
+  Serial.begin(9600);
+  connectToWiFi();
+  //connectToAWS();
+}
