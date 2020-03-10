51da6a8d83b056811a91d83ba40eb4d506080595
Mayur <55876994+mayurm03@users.noreply.github.com>
2020-03-10 15:25:44 +0000

    Add files via upload

---
+++ motor/motor.ino
@@ -0,0 +1,16 @@
+int motorPin = 14;
+ 
+void setup() {
+ 
+  pinMode(motorPin, OUTPUT);
+ 
+}
+ 
+void loop() {
+ 
+  digitalWrite(motorPin, HIGH);
+  delay(1000);
+  digitalWrite(motorPin, LOW);
+  delay(1000);
+ 
+}
---
+++ water_and_moisture/water_and_moisture.ino
@@ -0,0 +1,15 @@
+const int potPin = 14;
+
+int potValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+}
+
+void loop() {
+  // Reading potentiometer value
+  potValue = analogRead(potPin);
+  Serial.println(potValue);
+  delay(500);
+}
